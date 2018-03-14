6cc6f7b8548cbc242969365070570cd2ab1e2147
mc-luc <mishashan32@gmail.com>
2018-03-14 14:20:30 -0400

    'update'

--- Spill.java
+++ Spill.java
@@ -3,27 +3,33 @@ import static java.lang.System.*;
 
 public class Spill{
 	
+	// Grid Initial Parameters
+	public grid_x;
+	public grid_y;
+	public obstacle_count;
+	
 	//public int m, n, surface[m,n]
-	//public Grid surface;
+	public Grid surface;
+	
 	
 	private Spill(){
 		
+		// Grid Initial Parameters
+		grid_x = 10;
+		grid_y = grid_x;
+		obstacle_count = 3;
+		
 		//Initialise(surface)	// fills surface with initial values of CLEAR or OBSTACLE 
-		//surface = Grid( 10, 10, 3);
+		surface = new Grid( grid_x, grid_y, obstacle_count);
 		
 	}
 
-	public static void main(String[] args){
-	
-		print("Hello world");
-	
-	}
-
-	/*
-	void Spill(int x, int y, int strength) {
-		if strength =0 return 
-		If x<0 or x>=m or y<0 or y>=n return
-		int cell = surface(x,y)
+	public void Spill(int x, int y, int strength) {
+		
+		if (strength == 0) return;
+		if ( x < 0 || x >= grid_x || y < 0 || y >= grid_y ) return;
+		
+		Cell cell =  surface.getCell(x,y);
 		if cell is an OBSTACLE   return
 		If cell < strength  surface(x,y) = strength 	
 		Spill(x-1,y-1,strength-1)
@@ -34,10 +40,20 @@ public class Spill{
 		Spill(x+1,y-1,strength-1)
 		Spill(x+1,y,strength-1)
 		Spill(x+1,y+1,strength-1)
-	}*/
+	}
+	
+	public static void main(String[] args){
+	
+		print("Hello world");
+	
+	}
 	
 	private static void print(String string){
 		out.println(string);
+	}
+	
+	public String toString(){
+		out.println("Nothing yet");
 	}
 
 }
