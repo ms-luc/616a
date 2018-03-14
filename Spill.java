6f47795c73bd36da0a4d28bdfa6cd4c0f57a5776
mc-luc <mishashan32@gmail.com>
2018-03-14 13:43:50 -0400

    'update'

--- Spill.java
+++ Spill.java
@@ -2,19 +2,38 @@ import static java.lang.System.*;
 
 public class Spill{
 	
+	public int m, n, surface[m,n]
+	
 	private Spill(){
 		
+		Initialise(surface)	// fills surface with initial values of CLEAR or OBSTACLE 
 		
-	}
-	
-	private static void print(String string){
-		out.println(string);
 	}
 
 	public static void main(String[] args){
 	
 		print("Hello world");
 	
+	}
+
+	void Spill(int x, int y, int strength) {
+		if strength =0 return 
+		If x<0 or x>=m or y<0 or y>=n return
+		int cell = surface(x,y)
+		if cell is an OBSTACLE   return
+		If cell < strength  surface(x,y) = strength 	
+		Spill(x-1,y-1,strength-1)
+		Spill(x-1,y,strength-1)
+		Spill(x-1,y+1,strength-1)
+		Spill(x,y-1,strength-1)
+		Spill(x,y+1,strength-1)
+		Spill(x+1,y-1,strength-1)
+		Spill(x+1,y,strength-1)
+		Spill(x+1,y+1,strength-1)
+	}
+	
+	private static void print(String string){
+		out.println(string);
 	}
 
 }
